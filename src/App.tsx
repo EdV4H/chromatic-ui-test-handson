6eccd25aba99b7cabd729ccc3843d2f29aa64eff
EdV4H <circlemountain.ym@gmail.com>
2022-09-13 15:09:58 +0900

    fix: title

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <p>Hello Vite + React + Storybook + Chromatic!</p>
+      <p>Hello Vite + React + ESLint + Storybook + Chromatic!</p>
     </div>
   );
 }
